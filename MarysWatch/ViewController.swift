e03609129315dd696630f5237ff8fe5c0bbe5c40
FOGDevDamani <Damani@maryswatch.com>
2018-08-02 15:06:38 -0700

    Initial Commit

---
+++ MarysWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarysWatch
+//
+//  Created by Damani Turner on 8/2/18.
+//  Copyright © 2018 FOGDev Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
